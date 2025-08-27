e3beee4b01208aa3a9eff820158ec24a780a2f6c
Ivan K <Fox13613@gmail.com>
2025-08-27 21:26:30 +0200

    exe 6 test added

--- 1-JavaScript/Week2/assignment/ex6-totalCost.js
+++ 1-JavaScript/Week2/assignment/ex6-totalCost.js
@@ -5,29 +5,32 @@ const cartForParty = {
   pizza: 4.25,
   candy: 2.0,
 };
-
 function calculateTotalPrice(cart) {
   let total = 0;
-
   for (let key in cart) {
-    total += cart[key]; // cart["beers"], cart["chips"], ...
+    total += cart[key];
   }
-
   return `Total: €${total}`;
 }
-
 // ! Test functions (plain vanilla JavaScript)
 function test1() {
   console.log('\nTest 1: calculateTotalPrice should take one parameter');
+  const actual = calculateTotalPrice.length; // 1
+  const expected = 1;
+  console.assert(actual === expected);
 }
-
 function test2() {
-  console.log('\nTest 2: return correct output when passed cartForParty');
+  console.log('\nTest 2: return correct output when passe cartForParty');
+  const testCart = {
+    cola: 2,
+    kitkat: 2.4,
+    pepsi: 3,
+  };
+  const target = calculateTotalPrice(testCart);
+  console.assert(target === `Total: €7.4`);
 }
-
 function test() {
   test1();
   test2();
 }
-
 test();
